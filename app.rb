f9b40728212f3d309b30a09f28759e58694a9b09
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-08-31 13:46:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,9 +6,16 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
+  # post '/' do
+  #   text_from_user = params[:user_text]
 
+  #   erb :results
+  # end
+
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+  
     erb :results
   end
+
 end
